c07a80db7cede6f3795d2c3b984c661b83d092b2
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2020-05-18 12:05:14 +0200

    x

--- Learn-bpm-matlab/learnbpm.m
+++ Learn-bpm-matlab/learnbpm.m
@@ -5,13 +5,13 @@ load 'winequalityred.mat' %zaladowanie pliku z danymi
 
 %deklaracja stalych
 iterate = 0;
-S1 = 5;
-S2 = 3;
-lr = 0.01;
+S1 = 15;
+S2 = 9;
+lr = 0.005;
 mc = 0.9;
 
 x = length(S1)*length(S2)*length(lr)*length(mc);
-y = 6;
+y = 6; %liczba parametrow do zapisania w pliku
 Parameters = zeros(x, y);
 
 for i_S1 = 1:length(S1)
@@ -19,21 +19,21 @@ for i_S1 = 1:length(S1)
         for i_lr = 1:length(lr)
             for i_mc = 1:length(mc)
                 iterate = iterate + 1;
-                net = newff(Pn, T, [S1(i_S1), S2(i_S2)], {'logsig', 'logsig', 'purelin'},'traingdm', 'learngdm');
+                net = newff(Pns, Ts, [S1(i_S1), S2(i_S2)], {'tansig', 'tansig', 'purelin'},'traingdm', 'learngdm'); %logsig anlbo tansig
                 net.divideFcn = 'divideind';
-                net.divideParam.trainInd = 1:length(Pn);
-                net.divideParam.valInd = 1:length(Pn);
-                net.divideParam.testInd = 1:length(Pn);
+                net.divideParam.trainInd = 1:length(Pns);
+                net.divideParam.valInd = 1:length(Pns);
+                net.divideParam.testInd = 1:length(Pns);
                 net.trainParam.lr = lr(i_lr);
                 net.trainParam.mc = mc(i_mc);
-                net.trainParam.epochs = 20000;
+                net.trainParam.epochs = 100000;
                 net.trainParam.goal = 0.25;
-                net.performFcn = 'mse';
-                [net, tr, Y, E] = train(net, Pn, T);
-                a = sim(net, Pn);
+                net.performFcn = 'mse'; %mse
+                [net, tr, Y, E] = train(net, Pns, Ts);
+                a = sim(net, Pns);
                 
                 %zapis wyniku procentowego trenowania
-                currentPercent = (1 - sum(abs(T-a)>0.5)/length(P))*100;
+                currentPercent = (1 - sum(abs(Ts-a)>=0.5)/length(Pns))*100;
                 
                 %zapis wyników do tablicy
                 Parameters(iterate, 1) = iterate;
