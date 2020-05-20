eab70d961428ca7947aae81b52f6acd2f4e9914a
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2020-05-20 11:46:25 +0200

    up

--- Learn-bpm-matlab/learnbpm.m
+++ Learn-bpm-matlab/learnbpm.m
@@ -7,14 +7,9 @@ load 'winequalityred.mat' %zaladowanie pliku z danymi
 
 %deklaracja stalych
 iterate = 0;
-%S1 = (8:2:30);
-%S2 = (8:2:20);
-%lr = (0.01:-0.02:0.002);
-%mc = (0.7:0.1:0.9);
-
-S1 = (10:1:15);
-S2 = (9:1:14);
-lr = (0.01:-0.002:0.008);
+S1 = (8:2:30);
+S2 = (8:2:20);
+lr = [1e-1, 1e-2, 1e-3];
 mc = (0.8:0.1:0.9);
 
 x = length(S1)*length(S2)*length(lr)*length(mc);
@@ -27,21 +22,18 @@ for i_S1 = 1:length(S1)
         for i_lr = 1:length(lr)
             for i_mc = 1:length(mc)
                 iterate = iterate + 1;
-                net = newff(Pns, Ts, [S1(i_S1), S2(i_S2)], {'tansig', 'tansig', 'purelin'},'traingdm', 'learngdm'); %logsig albo tansig
-                net.divideFcn = 'divideind';
-                net.divideParam.trainInd = 1:length(Pns);
-                net.divideParam.valInd = 1:length(Pns);
-                net.divideParam.testInd = 1:length(Pns);
+                net = newff(Pn, T, [S1(i_S1), S2(i_S2)], {'tansig', 'tansig', 'purelin'},'traingdm', 'learngdm'); %logsig albo tansig
+                net.divideFcn = 'dividetrain';
                 net.trainParam.lr = lr(i_lr);
                 net.trainParam.mc = mc(i_mc);
-                net.trainParam.epochs = 10000;
+                net.trainParam.epochs = 20000;
                 net.trainParam.goal = 0.25;
-                net.performFcn = 'mse'; %mse
-                [net, tr, Y, E] = train(net, Pns, Ts);
-                a = sim(net, Pns);
+                net.performFcn = 'mse';
+                [net, tr, Y, E] = train(net, Pn, T);
+                a = sim(net, Pn);
                 
                 %zapis wyniku procentowego trenowania
-                currentPercent(i_S1, i_S2, i_lr, i_mc) = (1 - sum(abs(Ts-a)>=0.5)/length(Pns))*100;
+                currentPercent(i_S1, i_S2, i_lr, i_mc) = (1 - sum(abs(T-a)>=0.5)/length(Pn))*100;
                 
                 %zapis wyników do tablicy
                 Parameters(iterate, 1) = iterate;
