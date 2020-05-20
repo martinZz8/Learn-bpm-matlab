66bbc159cfbb473c571d49a1b59ea74355ed04f8
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2020-05-20 17:02:45 +0200

    x

--- Learn-bpm-matlab/learnbpm.m
+++ Learn-bpm-matlab/learnbpm.m
@@ -22,20 +22,20 @@ for i_S1 = 1:length(S1)
         for i_lr = 1:length(lr)
             for i_mc = 1:length(mc)
                 iterate = iterate + 1;
-                net = newff(Pn, T, [S1(i_S1), S2(i_S2)], {'tansig', 'tansig', 'purelin'},'traingdm', 'learngdm'); %logsig albo tansig
+                net = newff(Pns, Ts, [S1(i_S1), S2(i_S2)], {'tansig', 'tansig', 'purelin'},'traingdm', 'learngdm'); %logsig albo tansig
                 net.divideFcn = 'dividetrain';
                 net.trainParam.lr = lr(i_lr);
                 net.trainParam.mc = mc(i_mc);
                 net.trainParam.epochs = 10000;
                 net.trainParam.goal = 0.25;
                 net.performFcn = 'mse';
-                [net, tr, Y, E] = train(net, Pn, T);
-                a = sim(net, Pn);
+                [net, tr, Y, E] = train(net, Pns, Ts);
+                a = sim(net, Pns);
                 
                 %zapis wyniku procentowego trenowania
-                currentPercent(i_S1, i_S2, i_lr, i_mc) = (1 - sum(abs(T-a)>=0.5)/length(Pn))*100;
+                currentPercent(i_S1, i_S2, i_lr, i_mc) = (1 - sum(abs(Ts-a)>=0.5)/length(Pns))*100;
                 
-                %zapis wyników do tablicy
+                %zapis wynikow do tablicy
                 Parameters(iterate, 1) = iterate;
                 Parameters(iterate, 2) = S1(i_S1);
                 Parameters(iterate, 3) = S2(i_S2);
